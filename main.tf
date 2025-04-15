b4d699791a76e04cf10bd1680b77916a65044236
Veekshitha04 <veekshithareddymuthadi@gmail.com>
2025-04-15 15:24:21 +0530

    vv

--- main.tf
+++ main.tf
@@ -8,6 +8,7 @@ resource "azurerm_resource_group" "rg" {
     location = "canadacentral"
 }
 
+
 resource "azurerm_virtual_network" "vnet" {
   name                = "${var.prefix}-vnet"
   location            = azurerm_resource_group.rg.location
