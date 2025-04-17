65b5984368f89306a3a7a0746b9de16792239357
Veekshitha04 <veekshithareddymuthadi@gmail.com>
2025-04-17 13:33:45 +0530

    main change

--- main.tf
+++ main.tf
@@ -1,13 +1,12 @@
 variable "prefix" {
- default = "proj2"
- type = string
+  default = "veekshitha"
+  type    = string
 }
 
 resource "azurerm_resource_group" "rg" {
-    name = "${var.prefix}-rg"
-    location = "canadacentral"
+  name     = "${var.prefix}-rg"
+  location = "canadacentral"
 }
-
 
 resource "azurerm_virtual_network" "vnet" {
   name                = "${var.prefix}-vnet"
@@ -36,8 +35,6 @@ resource "azurerm_subnet" "subnet2" {
   resource_group_name  = azurerm_resource_group.rg.name
   virtual_network_name = azurerm_virtual_network.vnet.name
   address_prefixes     = ["10.0.0.64/26"]
-
-#   service_endpoints = ["Microsoft.Sql"]
 }
 
 resource "azurerm_app_service_plan" "asp" {
@@ -46,6 +43,7 @@ resource "azurerm_app_service_plan" "asp" {
   resource_group_name = azurerm_resource_group.rg.name
   kind                = "Linux"
   reserved            = true
+
   sku {
     tier = "Standard"
     size = "S1"
@@ -57,230 +55,94 @@ resource "azurerm_app_service" "as" {
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
   app_service_plan_id = azurerm_app_service_plan.asp.id
-  
+
   site_config {
-  linux_fx_version = "PYTHON|3.10"
-  always_on        = true
+    linux_fx_version = "PYTHON|3.10"
+    always_on        = true
   }
 
-  connection_string {
-    name  = "Database"
-    type  = "SQLServer"
-    value = "Server=tcp:proj2-sql.database.windows.net,1433;Initial Catalog=proj2-db;User ID=proj1_admin;Password=Proj@123#12;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;"
+  app_settings = {
+    "WEBSITE_RUN_FROM_PACKAGE" = "1"
+    "SQL_USER"     = "admin_user"
+    "SQL_SERVER"   = "${azurerm_mssql_server.server.name}.database.windows.net"
+    "SQL_DATABASE" = azurerm_mssql_database.db.name
+    "SQL_PASSWORD" = "Password@123#12"
+    "PYTHON_ENV"   = "production"
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+
+  lifecycle {
+    ignore_changes = [
+      source_control
+    ]
   }
 }
 
-
-/*
-resource "azurerm_app_service_slot" "slot" {
-  name                = "${var.prefix}-staging"
-  app_service_name    = azurerm_app_service.as.name
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
-  app_service_plan_id = azurerm_app_service_plan.asp.id
-}
-
-resource "azurerm_app_service_slot_virtual_network_swift_connection" "connect" {
-  slot_name      = azurerm_app_service_slot.slot.name
-  app_service_id = azurerm_app_service.as.id
-  subnet_id      = azurerm_subnet.subnet1.id
-}
-*/
-
-resource "azurerm_app_service_virtual_network_swift_connection" "integration" {
+resource "azurerm_app_service_virtual_network_swift_connection" "connect" {
   app_service_id = azurerm_app_service.as.id
   subnet_id      = azurerm_subnet.subnet1.id
 }
 
-resource "azurerm_app_service_source_control" "webapp_source" {
-  app_id             = azurerm_app_service.as.id
-  repo_url           = "https://github.com/Veekshitha04/webapp_py"
-  branch             = "main"
-  use_manual_integration = true
-  use_mercurial      = false
+resource "azurerm_mssql_server" "server" {
+  name                         = "${var.prefix}-sql"
+  resource_group_name          = azurerm_resource_group.rg.name
+  location                     = azurerm_resource_group.rg.location
+  version                      = "12.0"
+  administrator_login          = "admin_user"
+  administrator_login_password = "Password@123#12"
 }
 
-
-resource "azurerm_sql_server" "server" {
-    name = "${var.prefix}-sql"
-    resource_group_name = azurerm_resource_group.rg.name
-    location = azurerm_resource_group.rg.location
-    version = "12.0"
-    administrator_login = "proj1_admin"
-    administrator_login_password = "Proj@123#12"
-    
+resource "azurerm_mssql_database" "db" {
+  name           = "${var.prefix}-db"
+  server_id      = azurerm_mssql_server.server.id
+  collation      = "SQL_Latin1_General_CP1_CI_AS"
+  license_type   = "LicenseIncluded"
+  max_size_gb    = 2
+  sku_name       = "S0"
+  enclave_type   = "VBS"
 }
-
-resource "azurerm_sql_database" "db" {
-  name                = "${var.prefix}-db"
-  resource_group_name = azurerm_resource_group.rg.name
-  location            = azurerm_resource_group.rg.location
-  server_name         = azurerm_sql_server.server.name 
-}
-resource "azurerm_mssql_firewall_rule" "allow_azure_services" {
-  name             = "AllowAzureServices"
-  server_id        = azurerm_sql_server.server.id
-  start_ip_address = "0.0.0.0"
-  end_ip_address   = "0.0.0.0"
-}
-
-# resource "azurerm_sql_virtual_network_rule" "conn" {
-#   name                = "${var.prefix}-conn"
-#   resource_group_name = azurerm_resource_group.rg.name
-#   server_name         = azurerm_sql_server.server.name
-#   subnet_id           = azurerm_subnet.subnet2.id
-# }
 
 resource "azurerm_private_endpoint" "conn" {
   name                = "${var.prefix}-conn"
   resource_group_name = azurerm_resource_group.rg.name
   location            = azurerm_resource_group.rg.location
   subnet_id           = azurerm_subnet.subnet2.id
- 
+
   private_service_connection {
     name                           = "example-sql-connection"
-    private_connection_resource_id = azurerm_sql_server.server.id
+    private_connection_resource_id = azurerm_mssql_server.server.id
     is_manual_connection           = false
     subresource_names              = ["sqlServer"]
   }
 }
+
 resource "azurerm_private_dns_zone" "sql_dns" {
   name                = "privatelink.database.windows.net"
   resource_group_name = azurerm_resource_group.rg.name
 }
 
-resource "azurerm_private_dns_zone_virtual_network_link" "vnet_link" {
-  name                  = "${var.prefix}-dns-vnet-link"
+resource "azurerm_private_dns_zone_virtual_network_link" "sql_dns_link" {
+  name                  = "${var.prefix}-sdl"
   resource_group_name   = azurerm_resource_group.rg.name
   private_dns_zone_name = azurerm_private_dns_zone.sql_dns.name
   virtual_network_id    = azurerm_virtual_network.vnet.id
-  registration_enabled  = false
 }
 
 resource "azurerm_private_dns_a_record" "sql_a_record" {
-  name                = azurerm_sql_server.server.name
+  name                = azurerm_mssql_server.server.name
   zone_name           = azurerm_private_dns_zone.sql_dns.name
   resource_group_name = azurerm_resource_group.rg.name
   ttl                 = 300
   records             = [azurerm_private_endpoint.conn.private_service_connection[0].private_ip_address]
 }
-/*
-resource "azurerm_public_ip" "vm_pip" {
-  name                = "${var.prefix}-vm-pip"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
-  allocation_method   = "Static"
-  sku                 = "Standard"
+
+output "app_service_name" {
+  value = azurerm_app_service.as.name
 }
 
-resource "azurerm_network_interface" "vm_nic" {
-  name                = "proj2-nic"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
-
-  ip_configuration {
-    name                          = "internal"
-    subnet_id                     = azurerm_subnet.subnet2.id
-    private_ip_address_allocation = "Dynamic"
-    public_ip_address_id          = azurerm_public_ip.vm_pip.id
-  }
+output "resource_group_name" {
+  value = azurerm_resource_group.rg.name
 }
-
-resource "azurerm_linux_virtual_machine" "vm" {
-  name                = "${var.prefix}-vm"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
-  size                = "Standard_B1s"
-  admin_username      = "azureuser"
-  network_interface_ids = [
-    azurerm_network_interface.vm_nic.id,
-  ]
-
-  admin_ssh_key {
-    username   = "azureuser"
-    public_key = file("C:/Users/mureddy/.ssh/id_rsa.pub")
-  }
-
-  os_disk {
-    caching              = "ReadWrite"
-    storage_account_type = "Standard_LRS"
-    name                 = "${var.prefix}-osdisk"
-  }
-
-  source_image_reference {
-    publisher = "Canonical"
-    offer     = "0001-com-ubuntu-server-focal"
-    sku       = "20_04-lts"
-    version   = "latest"
-  }
-
-  disable_password_authentication = true
-}
-resource "azurerm_network_security_group" "vm_nsg" {
-  name                = "${var.prefix}-vm-nsg"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
-
-  security_rule {
-    name                       = "AllowSSH"
-    priority                   = 1001
-    direction                  = "Inbound"
-    access                     = "Allow"
-    protocol                   = "Tcp"
-    source_port_range          = "*"
-    destination_port_range     = "22"
-    source_address_prefix      = "*" # 👈 Change this to your IP for better security
-    destination_address_prefix = "*"
-  }
-  security_rule {
-    name                       = "AllowAppPort5000"
-    priority                   = 1002
-    direction                  = "Inbound"
-    access                     = "Allow"
-    protocol                   = "Tcp"
-    source_port_range          = "*"
-    destination_port_range     = "5000"
-    source_address_prefix      = "*"
-    destination_address_prefix = "*"
-  }
-
-}
-
-resource "azurerm_network_interface_security_group_association" "vm_nic_nsg_assoc" {
-  network_interface_id      = azurerm_network_interface.vm_nic.id
-  network_security_group_id = azurerm_network_security_group.vm_nsg.id
-}
-
-#bastion subnet
-resource "azurerm_subnet" "bastion_subnet" {
-  name                 = "AzureBastionSubnet" 
-  resource_group_name  = azurerm_resource_group.rg.name
-  virtual_network_name = azurerm_virtual_network.vnet.name
-  address_prefixes     = ["10.0.0.128/27"]
-}
-
-# Public IP for Bastion
-resource "azurerm_public_ip" "bastion_ip" {
-  name                = "${var.prefix}-bastion-pip"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
-  allocation_method   = "Static"
-  sku                 = "Standard"
-}
-
-# Azure Bastion Resource
-resource "azurerm_bastion_host" "bastion" {
-  name                = "bastion-host"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
-  sku                 = "Basic"
-
-  ip_configuration {
-    name                 = "bastion-config"
-    subnet_id            = azurerm_subnet.bastion_subnet.id
-    public_ip_address_id = azurerm_public_ip.bastion_ip.id
-  }
-}
-*/
-
-
--- provider.tf
+++ provider.tf
@@ -1,20 +1,24 @@
 # Configure the Azure provider, you can have many
 # if you use azurerm provider, it's source is hashicorp/azurerm
 # short for registry.terraform.io/hashicorp/azurerm
-
-
+ 
 terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 3.0.0"
+      version = "~> 4.12.0"
     }
   }
-
+ 
   required_version = ">= 1.9.0"
 }
 # configures the provider
-
+ 
 provider "azurerm" {
-  features {}
+  features {
+    resource_group{
+      prevent_deletion_if_contains_resources = false
+    }
+  }
+  subscription_id = "8e5ae5fe-f772-4899-9329-613723128161"
 }
